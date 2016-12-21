c6feee3d8a056bd253937d13d03dfdd2f8b9c89b
Simon Scherzinger <scherzinger@entrecode.de>
2016-12-21 09:34:14 +0100

    adds sinon and sinon-chai

--- test/basic.test.js
+++ test/basic.test.js
@@ -1,6 +1,10 @@
 'use stict';
 
 const chai = require('chai');
+const sinonChai = require('sinon-chai');
+
+chai.should();
+chai.use(sinonChai);
 
 describe('basic test of testing framework', () => {
   it('javascript should still be crazy', () => {
