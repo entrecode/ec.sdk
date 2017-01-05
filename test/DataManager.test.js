16c0324d9ddc3e76ead37fcbde3b6b146d07e216
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-05 14:15:01 +0100

    adds basic travis config

--- test/DataManager.test.js
+++ test/DataManager.test.js
@@ -4,6 +4,7 @@
 
 const chai = require('chai');
 const sinonChai = require('sinon-chai');
+const fs = require('fs');
 
 const DataManager = require('../lib/DataManager').default;
 const ListResource = require('../lib/ListResource').default;
@@ -12,13 +13,7 @@ const Resource = require('../lib/Resource').default;
 chai.should();
 chai.use(sinonChai);
 
-const token = 'eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNjaGVyemluZ2VyQGVudHJlY29kZS5kZ' +
-  'SIsImp0aSI6IjY5NDM1YzkwLTEyNzAtNDQ5OC1iMTE4LWVlNjcwM2VhYWVlNyIsImlhdCI6MTQ4MDE0NzYzOSwiZXhwIjo' +
-  'xNDgyNzM5NjM5LCJpc3MiOiJlbnRyZWNvZGUiLCJzdWIiOiJkZGQyOWZkMS03NDE3LTQ4OTQtYTU0Ni01YzEyYjExYzAxO' +
-  'DYifQ.EJkkbU1cb6KdjHgpFPrFYAqboRBhOsE2y4z92gD_YrrKUwjPpBXlhSFRalW1PWYW7wE6vUGI7eQDUqES1HZPgCXy' +
-  '5lWLMQOTV9fZcJnTodH4i4OfNcaVFoCrz3NjNlROgwKb3efrN3-gBHt_oj_g0sE9nSFMRnd84887YziaUfMBqd1wcUXRfd' +
-  '3JTx3jFBXmXvz44RaKZ-2X8WpQjF32bxWeYd69W60TWKzTaoTDFjRr8N6KsQSbB9tE7Db5Z5ZHwFtrj0SMdrl6bYIlJ_Ez' +
-  '9uA4F3ROOWvw4cB5n2rDzrmYWGZ6jft6h4bHnKE7moujAQMDCRcpo43gWsY8uz7-1A';
+const token = fs.readFileSync(`${__dirname}/token.txt`, 'utf-8').trim();
 
 describe('DataManager class', () => {
   it('instantiate', () => {
@@ -26,6 +21,7 @@ describe('DataManager class', () => {
   });
   it('should throw error', () => {
     const fn = () => {
+      /* eslint no-new:0 */
       new DataManager();
     };
     fn.should.throw(TypeError);
