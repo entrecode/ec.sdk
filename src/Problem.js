158b74bc0c2b31ddbe78e6cbd1146e844f5d9336
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-13 12:12:20 +0100

    adds todos on problem

--- src/Problem.js
+++ src/Problem.js
@@ -5,4 +5,12 @@ export default class Problem extends Error {
     super(error.title);
     Object.assign(this, error);
   }
+
+  toPrettyString(){
+    return `${this.title} - ${this.detail}`; // TODO beautify
+  }
 }
+
+// Error Code list
+// Code to pretty string representation
+// nested errors in embedded?
