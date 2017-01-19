634dd18930479e9e17331def0c87aaaa216389fb
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-19 16:20:34 +0100

    adds release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -e
+
+git diff-index --quiet HEAD --
+git checkout develop
+git checkout -b release/$1
+npm version --no-git-tag-version $1
+npm run docs
+npm run changelog
+git add CHANGELOG.md DOCUMENTATION.md package.json
+git commit -m "release version $1"
+git checkout master
+git merge --no-ff -m "merge release/$1 into master" release/$1
+git tag $1
+git push
+git checkout develop
+git merge --no-ff -m "merge release/$1 into develop" release/$1
+git push
+git branch -D release/$1
