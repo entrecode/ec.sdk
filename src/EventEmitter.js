a31a569086a4c311c1beafd98bfbd104c7f7da66
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-19 13:25:17 +0100

    adds EventEmitter jsdoc

--- src/EventEmitter.js
+++ src/EventEmitter.js
@@ -1,10 +1,26 @@
 'use strict';
 
+/**
+ * EventEmitter for emitting all received errors from any API.
+ *
+ * @class
+ * @access private
+ */
 class EventEmitter {
+  /**
+   * default constructor initialising a empty {@link Map} for event listeners.
+   */
   constructor() {
     this.listeners = new Map();
   }
 
+  /**
+   * Adds a listener for an event type.
+   *
+   * @param {string} label event type
+   * @param {function} callback function to invoke when event occurs.
+   * @returns {undefined}
+   */
   addListener(label, callback) {
     if (!this.listeners.has(label)) {
       this.listeners.set(label, []);
@@ -12,10 +28,24 @@ class EventEmitter {
     this.listeners.get(label).push(callback);
   }
 
+  /**
+   * Adds a listener for an event type.
+   *
+   * @param {string} label event type
+   * @param {function} callback function to invoke when event occurs.
+   * @returns {undefined}
+   */
   on(label, callback) {
     this.addListener(label, callback);
   }
 
+  /**
+   * Removes a listener.
+   *
+   * @param {string} label event type
+   * @param {function} callback listener function to remove.
+   * @returns {boolean} whether or not the listener got removed.
+   */
   removeListener(label, callback) {
     const listeners = this.listeners.get(label);
     const index = listeners.indexOf(callback);
@@ -30,10 +60,24 @@ class EventEmitter {
     return ret;
   }
 
+  /**
+   * Removes all listeners for a given label.
+   * @param {string} label event type.
+   * @returns {boolean} whether or not all listeners got removed.
+   */
   removeAllListeners(label) {
     return this.listeners.delete(label);
   }
 
+  /**
+   * Emits a new event. Calls all registered listeners.
+   *
+   * @access private
+   *
+   * @param {string} label event type
+   * @param {...*} args any number of arguments for this event
+   * @returns {boolean} whether or not a listener was called
+   */
   emit(label, ...args) {
     const listeners = this.listeners.get(label);
 
@@ -47,4 +91,8 @@ class EventEmitter {
   }
 }
 
+/**
+ * Global event emitter. All received errors will be emitted as an error event here.
+ * You can access this emitter with {@link DataManager#events} or {@link Accounts#events}
+ */
 export default new EventEmitter();
