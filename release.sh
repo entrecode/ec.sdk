657a304df636bbb0a73579afbcc4f2bd4852b4f0
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-20 13:12:49 +0100

    tag with vX.X.X so travis recognises the version

--- release.sh
+++ release.sh
@@ -13,7 +13,7 @@ git add CHANGELOG.md DOCUMENTATION.md package.json
 git commit -m "release version $1"
 git checkout master
 git merge --no-ff -m "merge release/$1 into master" release/$1
-git tag $1
+git tag v$1
 git push
 git checkout develop
 git merge --no-ff -m "merge release/$1 into develop" release/$1
