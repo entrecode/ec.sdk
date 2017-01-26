e91664487e263ac30abd4166689fa2e6a2a7808e
Simon Scherzinger <scherzinger@entrecode.de>
2017-01-26 12:44:11 +0100

    feat: export all Resources

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
-export { default as DataManager } from './DataManager';
 export { default as Accounts } from './Accounts';
+export { default as DataManager } from './DataManager';
+export { default as AccountList } from './resources/AccountList';
+export { default as AccountResource } from './resources/AccountResource';
+export { default as DataManagerList } from './resources/DataManagerList';
+export { default as DataManagerResource } from './resources/DataManagerResource';
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,6 @@
-export { DataManager } from './DataManager';
 export { Accounts } from './Accounts';
+export { DataManager } from './DataManager';
+export { AccountList } from './resources/AccountList';
+export { AccountResource } from './resources/AccountResource';
+export { DataManagerList } from './resources/DataManagerList';
+export { DataManagerResource } from './resources/DataManagerResource';
