42ee13e28d7af9e2cf549b24fc17bb32091d6248
Simon Scherzinger <scherzinger@entrecode.de>
2017-02-07 17:39:08 +0100

    fix: ModelResource typings fixed

--- typings/resources/ModelResource.d.ts
+++ typings/resources/ModelResource.d.ts
@@ -1,37 +1,37 @@
 import { Resource } from './Resource';
 
-export declare class AccountResource extends Resource {
+export declare class ModelResource extends Resource {
   constructor(resource: any, environment: string, traversal?: any);
 
-  setDescription(value: string): AccountResource;
+  setDescription(value: string): ModelResource;
 
   getDescription(): string;
 
-  setFields(value: Array<any>): AccountResource;
+  setFields(value: Array<any>): ModelResource;
 
   getFields(): Array<any>;
 
-  setHexColor(value: string): AccountResource;
+  setHexColor(value: string): ModelResource;
 
   getHexColor(): string;
 
-  setHooks(value: Array<any>): AccountResource;
+  setHooks(value: Array<any>): ModelResource;
 
   getHooks(): Array<any>;
 
-  setLocales(value: Array<string>): AccountResource;
+  setLocales(value: Array<string>): ModelResource;
 
   getLocales(): Array<string>;
 
-  setPolicies(value: Array<any>): AccountResource;
+  setPolicies(value: Array<any>): ModelResource;
 
   getPolicies(): Array<any>;
 
-  setTitle(value: string): AccountResource;
+  setTitle(value: string): ModelResource;
 
   getTitle(): string;
 
-  setTitleField(value: string): AccountResource;
+  setTitleField(value: string): ModelResource;
 
   getTitleField(): string;
 
