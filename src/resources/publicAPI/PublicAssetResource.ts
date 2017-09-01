25154bb4997e25e725c7cf712245711a8c430b1a
Simon Scherzinger <scherzinger@entrecode.de>
2017-09-01 15:34:10 +0200

    refactor: PublicAssetResource#resolve to Resource#resolve CMS-2927

--- src/resources/publicAPI/PublicAssetResource.ts
+++ src/resources/publicAPI/PublicAssetResource.ts
@@ -39,51 +39,6 @@ export default class PublicAssetResource extends Resource {
     super(resource, environment, traversal);
 
     this[resolvedSymbol] = 'tags' in this[resourceSymbol];
-
-    /*
-    Object.defineProperties(this, {
-      isResolved: {
-        enumerable: false,
-        get: () => this[resolvedSymbol],
-      },
-      assetID: {
-        enumerable: true,
-        get: () => this.getProperty('assetID'),
-      },
-
-      title: {
-        enumerable: true,
-        get: () => this.getProperty('title'),
-        set: (value) => {
-          this.setProperty('title', value);
-          return value;
-        },
-      },
-      created: {
-        enumerable: true,
-        get: () => new Date(this.getProperty('created')),
-      },
-      type: {
-        enumerable: true,
-        get: () => this.getProperty('type'),
-      },
-      files: {
-        enumerable: true,
-        get: () => this.getProperty('files'),
-      },
-    });
-    if (this.isResolved) {
-      Object.defineProperty(this, 'tags', {
-        enumerable: true,
-        get: () => this.getProperty('tags'),
-        set: (value) => {
-          this.setProperty('tags', value);
-          return value;
-        },
-      });
-    }
-    */
-
     this.countProperties();
   }
 
@@ -145,28 +100,7 @@ export default class PublicAssetResource extends Resource {
    * @returns {Promise<PublicAssetResource>} Promise resolving to {@link PublicAssetResource}.
    */
   resolve(): Promise<PublicAssetResource> {
-    if (this['isResolved']) {
-      return Promise.resolve(this);
-    }
-    return superagentGet(this.getLink('self').href, { Accept: 'application/json' }, this[environmentSymbol])
-    .then(resource => {
-      this[resourceSymbol] = halfred.parse(resource);
-      this[resolvedSymbol] = true;
-
-      /*
-      Object.defineProperty(this, 'tags', {
-        enumerable: true,
-        get: () => this.getProperty('tags'),
-        set: (value) => {
-          this.setProperty('tags', value);
-          return value;
-        },
-      });
-      this.countProperties();
-      */
-
-      return this;
-    });
+    return <Promise<PublicAssetResource>>super.resolve()
   }
 
   /**
