5df4f1eb90a3d32ac5014347af26ed66b067dd98
Simon Scherzinger <scherzinger@entrecode.de>
2017-09-04 13:07:14 +0200

    refactor: moved index into root directory

---
+++ index.ts
@@ -0,0 +1,5 @@
+export { default as Apps } from './src/Apps';
+export { default as Accounts } from './src/Accounts';
+export { default as DataManager } from './src/DataManager';
+export { default as PublicAPI } from './src/PublicAPI';
+export { default as Session } from './src/Session';
