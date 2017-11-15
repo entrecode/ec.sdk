fd1e4f2ed1ada7e44f61b0b845be82d0c0ee4bce
Simon Scherzinger <scherzinger@entrecode.de>
2017-11-15 14:53:39 +0100

    refactor: only add built sources for npm package, should allow angular production builds
    BREAKING CHANGE: import for typescript projects changed from `src` to `lib`.
    
    before:
    
    ```js
    import { EntryResource } from 'ec.sdk/src/resources/publicAPI/EntryResource';
    ```
    
    after:
    
    ```js
    import { EntryResource } from 'ec.sdk/lib/resources/publicAPI/EntryResource';
    ```

--- index.ts
+++
@@ -1,5 +0,0 @@
-export { default as Apps } from './src/Apps';
-export { default as Accounts } from './src/Accounts';
-export { default as DataManager } from './src/DataManager';
-export { default as PublicAPI } from './src/PublicAPI';
-export { default as Session } from './src/Session';
