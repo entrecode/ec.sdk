b7a71f9fe8a4edc413a9ba96467ea266f81a1707
Ruben Deyhle <deyhle@entrecode.de>
2022-07-12 12:27:39 +0200

    feat: account.createToken()

--- runme.js
+++ runme.js
@@ -1,8 +1,17 @@
-const { PublicAPI } = require('./lib/index');
+const { PublicAPI, Accounts } = require('./lib/index');
 
 (async () => {
-  const dm = new PublicAPI('beefbeef', 'stage').setToken('eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9...');
-
+  //const dm = new PublicAPI('beefbeef', 'stage').setToken('eyJhbGciOiJSUzUxMiIsInR5cCI6IkpXVCJ9...');
+  const accounts = new Accounts('stage').setToken('eyJhbGciOiJSUzUxMi....');
+  
+  const apiToken = await accounts.createApiToken();
+  console.log(apiToken);
+  const account = await accounts.account(apiToken.accountID);
+  console.log(account);
+  const newToken = await account.createToken();
+  console.log(newToken);
+  const allTokens = await account.tokenList();
+  console.log(allTokens);
   // TODO your magic
   
   return 'done';
